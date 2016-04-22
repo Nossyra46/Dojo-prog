f4e86fd03c695be8115d22041bdbefc864108403
Nossyra46 <nossyra@gmail.com>
2016-04-22 10:34:23 +0200

    hamming complete

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,23 +1,20 @@
 var Hamming = function() {};
 
 Hamming.prototype.compute = function(str1, str2) {
-	var hash = {
-		A: 'T',
-		T: 'A',
-		C: 'G',
-		G: 'C'
-	};
-
 	var hammingDistance = 0;
 
-	if (str1 === str2) {
-		hammingDistance = 0;
+	if (str1.length !== str2.length) {
+		throw new Error("DNA strands must be of equal length.")
 	}
 	else {
-		hammingDistance = 1;
-	}
+		for (var i = 0; i < str1.length; i++) {
+			if (str1[i] != str2[i]) {
+				hammingDistance ++;
+			}
+		}
 
-	return hammingDistance;
+		return hammingDistance;
+	}
 }
 
 module.exports = Hamming;
